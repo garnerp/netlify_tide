60b9cd84546c0267ccd00ef6b3e1e3ddab849ed9
garnerp <garnerp@gmail.com>
2016-08-09 14:25:36 -0400

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+
+grunt.initConfig({
+  critical: {
+    dist: {
+      options: {
+        base: './'
+      },
+      // The source file
+      src: 'http://brandcms4.mindtree.com/TidePage/Tide-home-async5.html',
+      // The destination file
+      dest: 'result.css'
+      }
+    }
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-critical');
+
+  // Default tasks.
+  grunt.registerTask('default', ['critical']);
+};
