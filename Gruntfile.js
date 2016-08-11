960673af2be4435ffb999a0d0c5e3d855d4662c5
garnerp <garnerp@gmail.com>
2016-08-11 14:19:11 -0400

    added a concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,47 @@
 module.exports = function (grunt) {
 
-grunt.initConfig({
-  critical: {
-    dist: {
+  grunt.initConfig({
+    concat: {
       options: {
-        base: './'
+        separator: ';',
       },
-      // The source file
-      src: 'dist/tide.html',
-      // The destination file
-      dest: 'dist/result.css'
+      dist: {
+        src: ['src/commonscript.js', 'src/desktop.js', 'src/siteroot.js'],
+        dest: 'dist/assets/js/all.js',
       }
     }
+
+//    uglify: {
+//      options: {
+//        banner: '/*! This is the Banner: <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+//        //banner: '/*! This is teh Banner <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+//      },
+//      build: {
+//        src: 'src/<%= pkg.name %>.js',
+//        dest: 'build/<%= pkg.name %>.min.js'
+//      }
+//    },
+    
+/*  
+  critical: {
+      dist: {
+        options: {
+          base: './'
+        },
+        // The source file
+        src: 'dist/tide.html',
+        // The destination file
+        dest: 'dist/result.css'
+      }
+    }
+*/    
   });
 
   // Load the plugins
   grunt.loadNpmTasks('grunt-critical');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default tasks.
-  grunt.registerTask('default', ['critical']);
-  //grunt.registerTask('default');
+  grunt.registerTask('default', ['concat']);
 };
