cdd4a62ec59bf399fcb4750dcffdf981512d1f0c
garnerp <garnerp@gmail.com>
2016-08-11 15:20:00 -0400

    fixed the copy to dist setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,42 +6,52 @@ module.exports = function (grunt) {
         separator: ';',
       },
       dist: {
-        src: ['src/commonscript.js', 'src/desktop.js', 'src/siteroot.js'],
+        src: ['src/js/commonscript.js', 'src/js/desktop.js', 'src/js/siteroot.js'],
         dest: 'dist/assets/js/all.js',
       }
-    }
+    },
 
-//    uglify: {
-//      options: {
-//        banner: '/*! This is the Banner: <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-//        //banner: '/*! This is teh Banner <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-//      },
-//      build: {
-//        src: 'src/<%= pkg.name %>.js',
-//        dest: 'build/<%= pkg.name %>.min.js'
-//      }
-//    },
-    
-/*  
-  critical: {
-      dist: {
-        options: {
-          base: './'
-        },
-        // The source file
-        src: 'dist/tide.html',
-        // The destination file
-        dest: 'dist/result.css'
-      }
-    }
-*/    
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'src',
+        src: '*.html',
+        dest: 'dist/',
+      },
+    },
+
+
+    //    uglify: {
+    //      options: {
+    //        banner: '/*! This is the Banner: <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //        //banner: '/*! This is teh Banner <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //      },
+    //      build: {
+    //        src: 'src/<%= pkg.name %>.js',
+    //        dest: 'build/<%= pkg.name %>.min.js'
+    //      }
+    //    },
+
+    /*  
+      critical: {
+          dist: {
+            options: {
+              base: './'
+            },
+            // The source file
+            src: 'dist/tide.html',
+            // The destination file
+            dest: 'dist/result.css'
+          }
+        }
+    */
   });
 
   // Load the plugins
   grunt.loadNpmTasks('grunt-critical');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
-
+  grunt.loadNpmTasks('grunt-contrib-copy');
   // Default tasks.
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['copy', 'concat']);
 };
